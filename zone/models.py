819a389e6a87c0d34aed848e15287fc0a363cdb2
RajPorus19 <55143326+RajPorus19@users.noreply.github.com>
2023-01-10 08:37:24 +0100

    creationg of model
    please keep in mind that the 0001_initial.py file is created when performing "python manage.py makemigration"

--- zone/models.py
+++ zone/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Zone(models.Model):
+    location = models.CharField(max_length=255, blank=True)
+    max_slots = models.IntegerField(blank=True)
+    occupied_slots = models.IntegerField(blank=True)
