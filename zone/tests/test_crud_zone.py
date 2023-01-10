51cf92cea419a7ba588908d43dbda00a859bf2cb
RajPorus19 <55143326+RajPorus19@users.noreply.github.com>
2023-01-10 09:03:14 +0100

    test now works for crud
    creation
    list view
    detail view
    update & delete
    are all tested !

--- zone/tests/test_crud_zone.py
+++ zone/tests/test_crud_zone.py
@@ -5,6 +5,7 @@ from zone.models import Zone
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.urls import reverse
+import json
 
 
 # Test unauthenticated user cannot create a Zone
@@ -36,6 +37,31 @@ class TestZoneCreateAuthenticatedTest(TestCase):
         # login the user
         self.client.login(username='testuser', password='12345')
 
-    def test_zone_create_authenticated(self):
-        response = self.client.post(self.url, {'location': '40.4167754,-3.7037901999999576','max_slots':30,'occupied_slots':15})
+    def test_zone_crud_authenticated(self):
+        # create / list view
+        data = {'location': '40.4167754,-3.7037901999999576','max_slots':30,'occupied_slots':15}
+        response = self.client.post(self.url, data)
         self.assertEqual(response.status_code, 201)
+
+        # read / list view
+        response = self.client.get(reverse('zone-list'))
+        self.assertEqual(len(response.json()),1) # one created
+        self.assertEqual(response.json()[0],data)
+
+        # read / detail view
+        id = Zone.objects.all().first().id
+        response = self.client.get(reverse('zone-detail',kwargs={'pk': id}))
+        self.assertEqual(response.json(),data)
+
+        # update / detail view
+        data['max_slots'] = 40
+        response = self.client.put(reverse('zone-detail',kwargs={'pk': id}),json.dumps(data),
+                                content_type="application/json")
+        self.assertEqual(response.json(),data)
+        self.assertEqual(response.json()['max_slots'],40)
+
+        # delete / detail view
+        response = self.client.delete(reverse('zone-detail',kwargs={'pk': id}))
+        response = self.client.get(reverse('zone-list'))
+        self.assertEqual(len(response.json()),0) # empty
+
