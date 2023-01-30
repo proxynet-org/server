8b1ec8d5da801cd173b23c4728890ebc272cba33
RajPorus19 <raj.porus.hiruthayaraj@gmail.com>
2023-01-30 14:58:45 +0100

    register user through api

--- proxynet_backend/settings.py
+++ proxynet_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u2!=+r%v$3&&#p7eaya02*$r#xub8#(z46d&y=zpc^h1xk%cd6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
