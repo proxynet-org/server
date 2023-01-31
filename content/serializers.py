57cab676248d0ba43d41811b53895965f594b908
hzmagm <59263934+hzmagm@users.noreply.github.com>
2023-01-31 22:04:09 +0100

    create a new hashed unique field

--- content/serializers.py
+++ content/serializers.py
@@ -5,10 +5,11 @@ from .models import Message
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
-        fields = ('user', 'text', 'created_at', 'updated_at', 'location')
+        fields = ('user', 'text', 'created_at', 'updated_at', 'location','idUser')
         extra_kwargs = {
             'user': {'read_only': True},
             'created_at': {'read_only': True},
             'updated_at': {'read_only': True},
             'location': {'read_only': True},
+            'idUser':{'read_only': True}
         }
