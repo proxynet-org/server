49fb0132695fc0e1ab25659f54d87162b32fd5af
RajPorus19 <raj.porus.hiruthayaraj@patrowl.io>
2023-04-30 09:44:53 +0200

    added content models to admin

--- content/admin.py
+++ content/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Message
+from .models import Message, PrivateMessage, Publication
 
 admin.site.register(Message)
+admin.site.register(PrivateMessage)
+admin.site.register(Publication)
