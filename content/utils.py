a4d85ba3a28b2912d68ab486ba875966f8210871
RajPorus19 <raj.porus.hiruthayaraj@patrowl.io>
2023-06-07 20:14:47 +0200

    try catch for none coordinates

--- content/utils.py
+++ content/utils.py
@@ -2,7 +2,10 @@ from geopy.distance import geodesic
 
 
 def get_distance_from_two_coordinates(coordinates1, coordinates2):
-    return geodesic(
-        (coordinates1["latitude"], coordinates1["longitude"]),
-        (coordinates2["latitude"], coordinates2["longitude"])
-    ).meters
+    try:
+        return geodesic(
+            (coordinates1["latitude"], coordinates1["longitude"]),
+            (coordinates2["latitude"], coordinates2["longitude"])
+        ).meters
+    except:
+        return 0
