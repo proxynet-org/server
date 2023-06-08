bf2634ef0d446df07a2288458d57ae730c50ad35
RajPorus19 <raj.porus.hiruthayaraj@patrowl.io>
2023-06-08 09:32:34 +0200

    admin page for comments

--- content/admin.py
+++ content/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Message, PrivateMessage, Publication
+from .models import Message, PrivateMessage, Publication, Comment
 
 admin.site.register(Message)
 admin.site.register(PrivateMessage)
 admin.site.register(Publication)
+admin.site.register(Comment)
